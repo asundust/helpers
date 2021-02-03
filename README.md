0747d9db8023a186db527b16dcf4716878dd746e
asundust <asundust@qq.com>
2021-02-03 15:38:22 +0800

    添加了新方法

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 <a href="https://packagist.org/packages/asundust/helpers"><img src="https://img.shields.io/packagist/dt/asundust/helpers" alt="Total Downloads"></a>
 <a href="https://packagist.org/packages/asundust/helpers"><img src="https://img.shields.io/packagist/v/asundust/helpers" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/asundust/helpers"><img src="https://img.shields.io/packagist/l/asundust/helpers" alt="License"></a>
 
 Laravel 快捷函数 / Quick Function for Laravel.
 
@@ -15,4 +14,4 @@ Laravel 快捷函数 / Quick Function for Laravel.
 
 ## License
 
-Laravel Helpers is open-sourced software licensed under the [MIT license](LICENSE.md).
+[The MIT License (MIT)](https://opensource.org/licenses/MIT) [MIT license](LICENSE.md).
--- src/helpers.php
+++ src/helpers.php
@@ -117,3 +117,58 @@ if (!function_exists('ql')) {
         fclose($handle);
     }
 }
+
+if (!function_exists('console_line')) {
+    /**
+     * 命令行模式中, 打印需要的数据.
+     *
+     * @param $text
+     * @param string $type
+     */
+    function console_line($text, $type = 'line')
+    {
+        if (app()->runningInConsole()) {
+            $types = [
+                'info' => 32, 'comment' => 33, 'warn' => 33, 'line' => 37, 'error' => '41;37', 'question' => '46;30',
+            ];
+            $code = $types[$type] ?? '37';
+            // 30黑色，31红色，32绿色，33黄色，34蓝色，35洋红，36青色，37白色，
+            echo chr(27).'['.$code.'m'."$text".chr(27).'[0m'.PHP_EOL;
+        }
+    }
+}
+
+if (!function_exists('console_info')) {
+    function console_info($text)
+    {
+        console_line($text, 'info');
+    }
+}
+
+if (!function_exists('console_comment')) {
+    function console_comment($text)
+    {
+        console_line($text, 'comment');
+    }
+}
+
+if (!function_exists('console_warn')) {
+    function console_warn($text)
+    {
+        console_line($text, 'warn');
+    }
+}
+
+if (!function_exists('console_error')) {
+    function console_error($text)
+    {
+        console_line($text, 'error');
+    }
+}
+
+if (!function_exists('console_question')) {
+    function console_question($text)
+    {
+        console_line($text, 'question');
+    }
+}
